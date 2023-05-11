5e56c9bd27eee09d4d25c3b36017a1f7a7443a1c
vinsky001 <ekibet544@gmail.com>
2023-05-11 02:21:07 -0700

    State Model - AirBnB

---
+++ models/state.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+This file defines  stateModel class
+"""
+
+
+from models.base_model import BaseModel
+
+
+class State(BaseModel):
+    # state model
+
+    # Atrributes
+    name: str = ''
