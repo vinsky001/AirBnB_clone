53167564ddfedd0b8d77e4757b78a963bfdac9e5
RIvAL-411 <cliffomanyo@gmail.com>
2023-05-14 08:39:11 -0700

    unittests task 4

--- tests/base_model_test.py
+++ tests/base_model_test.py
@@ -5,6 +5,7 @@
 import unittest
 from models.base_model import BaseModel
 from datetime import datetime
+import json
 
 
 class TestBaseModel(unittest.TestCase):
@@ -67,6 +68,44 @@ class TestBaseModel(unittest.TestCase):
         model_dict = self.model.to_dict()
         updated_at = model_dict['updated_at']
         self.assertEqual(updated_at, self.model.updated_at.isoformat())
+
+
+class TestBaseModelTwo(unittest.TestCase):
+    """ define unittests for base model two """
+
+    def setUp(self):
+        """ setup for proceeding tests two """
+        self.my_model = BaseModel()
+
+    def test_id_generation(self):
+        """ test for id gen type """
+        self.assertIsInstance(self.my_model.id, str)
+
+    def test_str_representation(self):
+        """ test for str rep """
+        expected = "[BaseModel] ({}) {}".format(self.my_model.id, self.my_model.__dict__)
+        self.assertEqual(str(self.my_model), expected)
+
+    def test_to_dict_method(self):
+        """ test for to dict method """
+        my_model_dict = self.my_model.to_dict()
+        self.assertIsInstance(my_model_dict['created_at'], str)
+        self.assertIsInstance(my_model_dict['updated_at'], str)
+        self.assertEqual(my_model_dict['__class__'], 'BaseModel')
+
+    def test_from_dict_method(self):
+        """ test for from dict method """
+        my_model_dict = self.my_model.to_dict()
+        my_new_model = BaseModel(**my_model_dict)
+        self.assertIsInstance(my_new_model, BaseModel)
+        self.assertEqual(my_new_model.id, self.my_model.id)
+        self.assertEqual(my_new_model.created_at, self.my_model.created_at)
+        self.assertEqual(my_new_model.updated_at, self.my_model.updated_at)
+
+    def test_created_at_and_updated_at_types(self):
+        """ test for created at and updated at types """
+        self.assertIsInstance(self.my_model.created_at, datetime)
+        self.assertIsInstance(self.my_model.updated_at, datetime)
 
 
 if __name__ == "__main__":
